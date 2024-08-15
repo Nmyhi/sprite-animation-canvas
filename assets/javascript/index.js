75d36ba1b97bd45cb238550d6a63a4fc66c42c75
Mihaly Lovrencsics <lomysa@gmail.com>
2024-08-15 21:23:01 +0000

    Add canvas values

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,1 +1,10 @@
-document.getElementById("")
+const canvas = document.getElementById("canvas1");
+const ctx = canvas.getContext('2d');
+
+
+const duck = new Image();
+duck.src = 'assets/prites/01.png';
+
+function animate() {
+    ctx.clearRect()
+};
